040f46989de9abf2acc4d2bb6927c7da3a673cd9
Anton Fedurtsya <anton@fedurtsya.com>
2024-05-26 11:39:37 +0300

    Create metadata.php

---
+++ metadata.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Copyright © MB Arbatos Klubas. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id' => 'fa_nutrition_facts',
+    'title' => 'Nutrition Facts',
+    'description' => [
+        'en' => 'Nutrition Facts module for OXID eShop.',
+    ],
+    'thumbnail' => 'logo.png',
+    'version' => '0.1.0',
+    'author' => 'Anton Fedurtsya',
+    'email' => 'anton@fedurtsya.com',
+    'url' => 'https://github.com/Fresh-Advance',
+    'controllers' => [
+    ],
+    'extend' => [
+    ],
+    'settings' => [
+    ],
+    'events' => [
+    ],
+    'templates' => [
+    ]
+];
