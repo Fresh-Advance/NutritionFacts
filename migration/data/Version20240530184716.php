fc612c921f710c81d0c36e4fc2fec7099264382e
Anton Fedurtsya <anton@fedurtsya.com>
2024-05-30 22:46:27 +0300

    Create table for product nutrition facts
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ migration/data/Version20240530184716.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FreshAdvance\NutritionFacts\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240530184716 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            "CREATE TABLE fa_nutrition_facts (
+                product_id VARCHAR(32),
+                language_id INT,
+                nutrition_facts JSON,
+                PRIMARY KEY (product_id, language_id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
