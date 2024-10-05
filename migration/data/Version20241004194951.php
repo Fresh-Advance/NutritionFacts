02c118f404bb1cca3a3e8cd4b0352d6131a87b74
Anton Fedurtsya <anton@fedurtsya.com>
2024-10-05 19:34:03 +0300

    Create tables
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ migration/data/Version20241004194951.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FreshAdvance\NutritionFacts\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241004194951 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            "CREATE TABLE fa_nutrition_facts (
+                product_id VARCHAR(32),
+                nutrition_facts LONGTEXT,
+                PRIMARY KEY (product_id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+        );
+
+        $this->addSql(
+            "CREATE TABLE fa_nutrition_facts_relations (
+                product_id VARCHAR(32),
+                related_product_id VARCHAR(32),
+                INDEX (product_id) 
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
