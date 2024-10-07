15c5ed53324277e6521fd6f653dc17e1a4cbe6a0
Anton Fedurtsya <anton@fedurtsya.com>
2024-10-07 20:28:44 +0300

    NFM-2 Save nutrition facts from controller
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ src/Service/FactsService.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © MB Arbatos Klubas. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace FreshAdvance\NutritionFacts\Service;
+
+use FreshAdvance\NutritionFacts\DataType\ProductFacts;
+use FreshAdvance\NutritionFacts\DataType\ProductFactsInterface;
+use FreshAdvance\NutritionFacts\DataTypeFactory\FactsDataFactoryInterface;
+use FreshAdvance\NutritionFacts\DataTypeFactory\NutritionFactsFactoryInterface;
+use FreshAdvance\NutritionFacts\Repository\FactsDataAccessInterface;
+
+class FactsService implements FactsServiceInterface
+{
+    public function __construct(
+        protected FactsDataAccessInterface $factsDataAccess,
+        protected NutritionFactsFactoryInterface $nutritionFactsFactory,
+        protected FactsDataFactoryInterface $factsDataFactory,
+    ) {
+    }
+
+    public function getProductFacts(string $productId): ProductFactsInterface
+    {
+        $factsData = $this->factsDataAccess->getFactsData($productId);
+
+        return new ProductFacts(
+            title: '',
+            nutritionFacts: $this->nutritionFactsFactory->getFromArray($factsData->getNutritionFactsData()),
+        );
+    }
+
+    public function saveProductFacts(string $productId, ProductFactsInterface $productFacts): void
+    {
+        $factsData = $this->factsDataFactory->getFromProductFacts($productFacts);
+        
+        $this->factsDataAccess->saveFactsData($productId, $factsData);
+    }
+}
---
+++ tests/Unit/Service/FactsServiceTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Copyright © MB Arbatos Klubas. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace FreshAdvance\NutritionFacts\Tests\Unit\Service;
+
+use FreshAdvance\NutritionFacts\DataType\FactsDataInterface;
+use FreshAdvance\NutritionFacts\DataType\NutritionFactsInterface;
+use FreshAdvance\NutritionFacts\DataType\ProductFactsInterface;
+use FreshAdvance\NutritionFacts\DataTypeFactory\FactsDataFactoryInterface;
+use FreshAdvance\NutritionFacts\DataTypeFactory\NutritionFactsFactoryInterface;
+use FreshAdvance\NutritionFacts\Repository\FactsDataAccessInterface;
+use FreshAdvance\NutritionFacts\Service\FactsService;
+use FreshAdvance\NutritionFacts\Service\FactsServiceInterface;
+use PHPUnit\Framework\TestCase;
+
+class FactsServiceTest extends TestCase
+{
+    public function testGetProductFacts(): void
+    {
+        $productId = uniqid();
+
+        $sut = $this->getSut(
+            factsDataAccess: $factsDataAccessMock = $this->createMock(FactsDataAccessInterface::class),
+            nutritionFactsFactory: $nutritionFactsFactoryMock = $this->createMock(NutritionFactsFactoryInterface::class),
+        );
+
+        $factsDataAccessMock->method('getFactsData')
+            ->with($productId)
+            ->willReturn($factsDataStub = $this->createStub(FactsDataInterface::class));
+
+        $nutritionFactsFactoryMock->method('getFromArray')
+            ->with($factsDataStub->getNutritionFactsData())
+            ->willReturn($nutritionFactsStub = $this->createStub(NutritionFactsInterface::class));
+
+        $productFacts = $sut->getProductFacts($productId);
+
+        $this->assertSame($nutritionFactsStub, $productFacts->getNutritionFacts());
+    }
+
+    public function testSaveProductFacts(): void
+    {
+        $productId = uniqid();
+
+        $sut = $this->getSut(
+            factsDataAccess: $factsDataAccessMock = $this->createMock(FactsDataAccessInterface::class),
+            factsDataFactory: $factsDataFactoryMock = $this->createMock(FactsDataFactoryInterface::class),
+        );
+
+        $productFactsStub = $this->createStub(ProductFactsInterface::class);
+        $factsDataFactoryMock->method('getFromProductFacts')
+            ->with($productFactsStub)
+            ->willReturn($factsDataStub = $this->createStub(FactsDataInterface::class));
+
+        $factsDataAccessMock->expects($this->once())
+            ->method('saveFactsData')
+            ->with($productId, $factsDataStub)
+            ->willReturn(true);
+
+        $sut->saveProductFacts($productId, $productFactsStub);
+    }
+
+    public function getSut(
+        FactsDataAccessInterface $factsDataAccess = null,
+        NutritionFactsFactoryInterface $nutritionFactsFactory = null,
+        FactsDataFactoryInterface $factsDataFactory = null,
+    ): FactsServiceInterface
+    {
+        return new FactsService(
+            factsDataAccess: $factsDataAccess ?? $this->createStub(FactsDataAccessInterface::class),
+            nutritionFactsFactory: $nutritionFactsFactory ?? $this->createStub(NutritionFactsFactoryInterface::class),
+            factsDataFactory: $factsDataFactory ?? $this->createStub(FactsDataFactoryInterface::class),
+        );
+    }
+}
